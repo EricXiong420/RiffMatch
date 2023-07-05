787b62add029b7a0f836c08b177c4c7968248cad
officialchengyud <39908329+officialchengyud@users.noreply.github.com>
2023-07-05 16:46:10 +1200

    Fixed error

--- api/matches.js
+++ api/matches.js
@@ -39,18 +39,20 @@ export const GetUserProfilesFromUUIDs = async (uuids, callback) => {
     const batch = uuids.splice(0, 10);
 
     // add the batch request to to a queue
-    batches.push(
-      firestore()
-        .collection("users")
-        .where("uuid", "in", [...batch])
-        .get()
-        .then((results) =>
-          results.docs.map((result) => ({
-            id: result.id,
-            ...result.data(),
-          }))
-        )
-    );
+    if (batch.length > 0) {
+      batches.push(
+        firestore()
+          .collection("users")
+          .where("uuid", "in", [...batch])
+          .get()
+          .then((results) =>
+            results.docs.map((result) => ({
+              id: result.id,
+              ...result.data(),
+            }))
+          )
+      );
+    }
   }
 
   // after all of the data is fetched, return it
